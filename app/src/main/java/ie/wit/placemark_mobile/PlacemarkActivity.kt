f1c5840a2ec902371749452dc1bef48e62c32e9e
Arunasjok <jokubynas@gmail.com>
2022-09-11 19:57:36 +0100

    Event Handling

--- app/src/main/java/ie/wit/placemark_mobile/PlacemarkActivity.kt
+++ app/src/main/java/ie/wit/placemark_mobile/PlacemarkActivity.kt
@@ -2,16 +2,37 @@ package ie.wit.placemark_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.wit.placemark_mobile.databinding.ActivityPlacemarkBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
+private lateinit var binding: ActivityPlacemarkBinding
+
 class PlacemarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlacemarkBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placemark)
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
 
         i("Placemark Activity started..")
+
+        binding.btnAdd.setOnClickListener() {
+            val placemarkTitle = binding.placemarkTitle.text.toString()
+            if (placemarkTitle.isNotEmpty()) {
+                i("add Button Pressed: $placemarkTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
